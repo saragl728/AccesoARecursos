92f6ff94ad01f8ac69dd5abc1b7299d929a8f65f
Usuario <saragl728@gmail.com>
2024-01-15 09:44:49 +0100

    Añado algo

--- app/src/main/java/com/example/accesoarecursos/MainActivity.kt
+++ app/src/main/java/com/example/accesoarecursos/MainActivity.kt
@@ -29,6 +29,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         //se llama a un launcher que pide que se seleccione una imagen
